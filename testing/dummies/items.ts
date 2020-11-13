76d62094b5e35d6a9a8c5353a455e6615a29ff92
Stiven Sierra <stivensierra@gmail.com>
2020-11-12 19:15:02 -0500

    Update items.ts

--- testing/dummies/items.ts
+++ testing/dummies/items.ts
@@ -1,5 +1,5 @@
 export const ALL_TEMS = [    
-    { "id": 1, "brand": "perro", "url": "assets/images/perro1.jpg" },    
+    { "id": 1, "brand": "perro2", "url": "assets/images/perro1.jpg" },    
     { "id": 2, "brand": "perro", "url": "assets/images/perro2.jpg" },
     { "id": 3, "brand": "gato", "url": "assets/images/gato1.jpg" },
     { "id": 4, "brand": "gato", "url": "assets/images/gato2.jpeg" },
